5380d3315dfe2d5dac84304a97d069d482e55504
KeRon-asm <clark.keron.e@gmail.com>
2026-03-31 23:57:29 -0400

    Add files via upload
    Beginning part 3: Memory Simulation Hierarchy

---
+++ Memory_Hierarchy_Simulation.py
@@ -0,0 +1,12 @@
+"""
+First step:
+Implement SSD, DRAM, and CACHE hierearchy, (L3, L2,L1)
+
+What must be enforced:
+-Data Flow
+-Direct access and bypassing is prohibited
+-All data must be treated as 32-bit instructions
+
+Workable plan:
+Implement each component as an object.
+"""
